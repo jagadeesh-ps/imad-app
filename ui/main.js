2d5ee0b3c85ad5d79e10d2194e02bf0ee1e99589
jagadeesh-ps <30662460+jagadeesh-ps@users.noreply.github.com>
2017-08-13 21:00:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 //add new value to the element
 var element = document.getElementById("main-text");
 element.innerHTML = "New Value";
+
+var img = document.getElementById("madi");
+img.onclick = function () {
+    img.style.marginLeft = '100px';
+}
