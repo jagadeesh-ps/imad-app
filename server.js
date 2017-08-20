a481f6dce6d50439e6fdd2b73ccc6e67e20107c5
jagadeesh-ps <30662460+jagadeesh-ps@users.noreply.github.com>
2017-08-20 18:06:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,9 +75,13 @@ app.get('/:articleName', function(req, res){
    //articleName == article-one
    //articles[articleName] == {} content object for article one
    var articleName = req.params.articleName;
-   res.send(createTemplate(articles[articleName]))
+   res.send(createTemplate(articles[articleName]));
 });
 
+app.get('/counter', function(req, res){
+   counter = counter + 1;
+   res.sent(counter.toString());
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
